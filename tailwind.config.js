67524b8d740b2f61c64274f00fdfa91eeee5267f
Sulabh Shrestha <Shresthasulabh777@gmail.com>
2023-08-13 21:45:01 +0545

    Theme added to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "screen-bg": "var(--screen-bg)",
+        "main-bg": "var(--main-bg)",
+        "keypad-bg": "var(--keypad-bg)",
+        "key-bg": "var(--key-bg)",
+        "equal-key-background": "var(--equal-key-background)",
+        "equal-key-shadow": "var(--equal-key-shadow)",
+        "func-key-background": "var(--func-key-background)",
+        "func-key-shadow": "var(--func-key-shadow)",
+        "dark--text-color": "var(--dark--text-color)",
+        "white-text-color": "var(--white-text-color)",
+        "light-yellow-text-color": "var(--light-yellow-text-color)",
+      },
+    },
   },
   plugins: [require("daisyui")],
 };
